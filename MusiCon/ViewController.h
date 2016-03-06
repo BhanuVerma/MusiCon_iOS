7731f945c8725e745ebe6fd52fbbb0bd0f5a065e
Bhanu Verma <bhanu.iiitd@gmail.com>
2016-03-06 15:23:32 -0500

    Initial Commit

---
+++ MusiCon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusiCon
+//
+//  Created by Bhanu Verma on 3/6/16.
+//  Copyright © 2016 theRecommendables. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusiCon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusiCon
+//
+//  Created by Bhanu Verma on 3/6/16.
+//  Copyright © 2016 theRecommendables. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
